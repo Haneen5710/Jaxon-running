562e242e14b5f39f00ff7f7e2d2d60d20a5b8998
Haneen5710 <92688513+Haneen5710@users.noreply.github.com>
2021-11-05 20:20:45 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,32 +4,36 @@ var rightEdge,leftEdge;
 function preload(){
   //pre-load images
   pathImg = loadImage("path.png");
-  runnerImg=loadAnimation("path.png");
-runnerImg=loadAnimation("Runner-1.png,Runner-2.png");
+runnerImg=loadAnimation("Runner-1.png","Runner-2.png");  
 }
 
 function setup(){
   createCanvas(400,400);
   //create sprites here
-  path=createSpite(200,200);
+  path=createSprite(200,200);
   path.addImage(pathImg);
 path.velocityY = 4;
 path.scale=1.2;
-if(path.y > 400 ){
-  path.y = height/2;
-  runner.addImage(runnerImg);
-
-}
+rightEdge = createSprite(410,0,100,800);
+rightEdge.visibile=false;
+ leftEdge = createSprite(0,0,100,800);
+ leftEdge.visibile=false;
+runner=createSprite(180,340,30,30)
+runner.addAnimation("Jackson",runnerImg);
+runner.scale=0.08;
 }
 
 function draw() {
   background(0);
- rightEdge = createSpite(400,0,20,400);
- leftEdge = createSpite(0,0,20,400);
+  path.velocityY=4;
   runner.collide(rightEdge,leftEdge);
-  runner.y = Word.mouseY
-
- 
-
-
+  runner.x = World.mouseX
+  edge=createEdgeSprites();
+  runner.collide(edge[3]);
+  runner.collide(leftEdge);
+  runner.collide(rightEdge);
+  if(path.y > 400 ){
+    path.y = height/2;
+  }
+  drawSprites();
 }
